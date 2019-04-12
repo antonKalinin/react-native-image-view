6e13432a2f5156c38a04c1d99d9084ffe6e31a05
長沼和歌子 <w.naganuma@getgamba.com>
2019-04-12 15:35:39 +0900

    [#165049326]テスト用コンソール出力追加

--- src/ImageView.js
+++ src/ImageView.js
@@ -235,7 +235,7 @@ export default class ImageView extends Component<PropsType, StateType> {
             scale: 1,
             translate: {},
         };
-        console.log('in onNextImageReceived')
+        console.warn('in onNextImageReceived')
 
         this.setState({
             images,
@@ -287,7 +287,7 @@ export default class ImageView extends Component<PropsType, StateType> {
             const nextImageScale = this.getInitialScale(nextImageIndex);
             const nextImageTranslate = this.getInitialTranslate(nextImageIndex);
 
-            console.log('in onNextImage')
+            console.warn('in onNextImage')
             this.setState({
                 imageIndex: nextImageIndex,
                 imageScale: nextImageScale,
